551d73667831aa9f8bf7a455b12e61a98071f400
Dicky Pratama <dicky.pratama@arthasemesta.com>
2024-11-15 17:45:20 +0700

    first init and finalize the feature

---
+++ constants/contant.go
@@ -0,0 +1,12 @@
+package constants
+
+const (
+	SuccessMessage      = "success"
+	ErrFailedBadRequest = "data tidak sesuai"
+	ErrServerError      = "terjadi kesalahan pada server"
+)
+
+const (
+	RedisKeyProducts      = "ecommerce:products"
+	RedisKeyProductDetail = "ecommerce:product:%d"
+)
---
+++ internal/repository/category.go
@@ -0,0 +1,37 @@
+package repository
+
+import (
+	"context"
+	"ecommerce-product/internal/models"
+
+	"gorm.io/gorm"
+)
+
+type CategoryRepo struct {
+	DB *gorm.DB
+}
+
+func (r *CategoryRepo) InsertNewCategory(ctx context.Context, category *models.ProductCategory) error {
+	return r.DB.Create(category).Error
+}
+
+func (r *CategoryRepo) UpdateCategory(ctx context.Context, categoryID int, newData map[string]interface{}) error {
+	err := r.DB.Debug().Model(&models.ProductCategory{}).Where("id = ?", categoryID).Updates(newData).Error
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (r *CategoryRepo) DeleteCategory(ctx context.Context, categoryID int) error {
+	return r.DB.Exec("DELETE FROM product_categories WHERE id = ?", categoryID).Error
+}
+func (r *CategoryRepo) GetCategory(ctx context.Context) ([]models.ProductCategory, error) {
+	var (
+		resp []models.ProductCategory
+		err  error
+	)
+	err = r.DB.Find(&resp).Error
+	return resp, err
+}
